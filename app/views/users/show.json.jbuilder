280b4063b030e027131bc270e4e06d80c5d2e190
suwiding <suwiding@gmail.com>
2014-06-04 20:54:13 -0400

    first commit of rails directory app

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :firstname, :lastname, :age, :email, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Directory::Application.config.secret_key_base = '6361ac786857b1753d1e3706032f0f31e64131a2218a21ba3328179c4e09289d697f7a4fdfa44f8abe06f0eb5294cee5c824605dd97b92d48ac62fcf926c76d9'
